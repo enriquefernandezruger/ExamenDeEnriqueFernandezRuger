93ffa4466c611db6a1fbd3648e7bb82f5a194bd7
HP-Enrique <HP-Enrique@172.16.0.106>
2018-04-19 08:49:06 +0200

    Estado Inicial

---
+++ ExamenDeEnriqueFR_GitHub/src/examendeenriquefr/ExamenDeEnriqueFernandezRuger.java
@@ -0,0 +1,10 @@
+package examendeenriquefr;
+
+public class ExamenDeEnriqueFernandezRuger {
+
+	public static void main(String[] args) {
+		System.out.println("La fecha de hoy es: 19/04/18");
+
+	}
+
+}
